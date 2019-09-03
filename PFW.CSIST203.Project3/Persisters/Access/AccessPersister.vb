9a3c7f44a890e3dfa45c2e50a86a7ed0c7eba818
Jonathon Gottschalk <gottjl01@pfw.edu>
2019-09-02 23:42:28 -0400

    Initial Project Add

---
+++ PFW.CSIST203.Project3/Persisters/Access/AccessPersister.vb
@@ -0,0 +1,54 @@
+﻿Namespace PFW.CSIST203.Project3.Persisters.Access
+
+    ''' <summary>
+    ''' Access persister that interacts with an Access database
+    ''' </summary>
+    Public Class AccessPersister
+        Implements IPersistData
+
+        Friend ReadOnly accessFile As String = Nothing
+        Friend noDatabase As Boolean = False
+        Friend IsDisposed As Boolean = False
+
+        Public Sub New()
+            'TODO: Implement
+        End Sub
+
+        Public Sub New(accessFile As String)
+            Throw New NotImplementedException()
+        End Sub
+
+        ''' <summary>
+        ''' The filter used by the open dialog to find files that this persister will handle
+        ''' </summary>
+        ''' <returns></returns>
+        Public ReadOnly Property FileFilter As String Implements IPersistData.FileFilter
+            Get
+                Return "Access Database Files|*.mdb;*.accdb"
+            End Get
+        End Property
+
+        Public Function GetRow(id As Integer) As DataRow Implements IPersistData.GetRow
+            Return Nothing
+        End Function
+
+        Public Function CountRows() As Integer Implements IPersistData.CountRows
+            Return 0
+        End Function
+
+        Public Sub Dispose() Implements IDisposable.Dispose
+            ' TODO: Implement
+        End Sub
+
+        Public Function GetData() As DataTable Implements IPersistData.GetData
+            Return New DataTable()
+        End Function
+
+        Public Sub UpdateRow(row As DataRow) Implements IPersistData.UpdateRow
+            Throw New NotImplementedException()
+        End Sub
+
+    End Class
+
+End Namespace
+
---
+++ PFW.CSIST203.Project3/Persisters/IPersistData.vb
@@ -0,0 +1,45 @@
+﻿Namespace PFW.CSIST203.Project3.Persisters
+
+    ''' <summary>
+    ''' Generic interface used for persisting data to and from a data source
+    ''' </summary>
+    Public Interface IPersistData
+        Inherits IDisposable
+
+        ''' <summary>
+        ''' Retrieves a specific row number from the data source using a unique ID
+        ''' </summary>
+        ''' <param name="id">The unique identifier used by this persister to retrieve specific rows</param>
+        ''' <returns>The data row representing the requested data</returns>
+        Function GetRow(id As Integer) As System.Data.DataRow
+
+        ''' <summary>
+        ''' Retrieves a count of the number of elements present in the data source
+        ''' </summary>
+        ''' <returns>The number of items present in the underlying data source</returns>
+        Function CountRows() As Integer
+
+        ''' <summary>
+        ''' Retrieves all data belonging to the data source as a .NET DataTable
+        ''' </summary>
+        ''' <returns>An in-memory representation of the data source's data as a data table</returns>
+        Function GetData() As DataTable
+
+        ''' <summary>
+        ''' Updates the data row belonging to the underlying data source
+        ''' </summary>
+        ''' <param name="row">The data row that should be updated with all modifications made</param>
+        Sub UpdateRow(row As System.Data.DataRow)
+
+        ''' <summary>
+        ''' The file filter used by this persister when browsing for a file
+        ''' </summary>
+        ''' <returns>A valid string used by the OpenDialog browsing control</returns>
+        ReadOnly Property FileFilter As String
+
+
+    End Interface
+
+End Namespace
+
+
